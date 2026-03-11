26bbacbf4dda46c5690c9e9539323f797b89f20b
NimbleMike <berhan@hpe.com>
2026-03-11 15:39:12 -0700

    Sync

--- src/main/groovy/com/morpheusdata/scvmm/ScvmmConstants.groovy
+++ src/main/groovy/com/morpheusdata/scvmm/ScvmmConstants.groovy
@@ -9,13 +9,15 @@ import java.util.regex.Pattern
 @CompileStatic
 class ScvmmConstants {
     /**
-     * SCVMM can create temporary VM templates with names like "Temporary Template{UUID}" during provisioning or
-     * template-based deployment operations. These templates are intended to be short-lived and are normally removed
-     * automatically when the operation completes. However, if an operation fails or is interrupted, temporary
-     * templates may remain in SCVMM and clutter template listings. This regex/pattern allows the plugin to identify
-     * and ignore those leftovers.
+     * SCVMM can create temporary VM templates with names like "Temporary Template{UUID}" while the SCVMM plugin can
+     * create temporary VM templates with names like "Temporary Morpheus Template{UUID}". These occur during
+     * provisioning or template-based deployment operations. These templates are intended to be short-lived and are
+     * normally removed automatically when the operation completes. However, if an operation fails or is interrupted,
+     * temporary templates may remain in SCVMM and clutter template listings. This regex/pattern allows the plugin to
+     * identify and ignore those leftovers.
      */
     static final String TEMPORARY_TEMPLATE_UUID_REGEX =
-            '^Temporary Template\\s*[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
+            '^Temporary (?:Morpheus )?Template\\s*' +
+                    '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
     static final Pattern TEMPORARY_TEMPLATE_UUID_PATTERN = Pattern.compile(TEMPORARY_TEMPLATE_UUID_REGEX)
 }
