7abffd75e78d718e26bec158a250a67fc52d46a1
NimbleMike <berhan@hpe.com>
2026-06-08 11:06:03 -0700

    Remove redudant "...success = false" due to setError usage

--- src/main/groovy/com/morpheusdata/scvmm/ScvmmProvisionProvider.groovy
+++ src/main/groovy/com/morpheusdata/scvmm/ScvmmProvisionProvider.groovy
@@ -644,7 +644,6 @@ class ScvmmProvisionProvider extends AbstractProvisionProvider implements Worklo
                     if (cloudFiles?.size() == 0) {
                         server.statusMessage = 'Failed to find cloud files'
                         provisionResponse.setError("Cloud files could not be found for ${virtualImage}")
-                        provisionResponse.success = false
                     }
                     def containerImage = [
                             name          : virtualImage.name ?: workload.workloadType.imageCode,
@@ -2384,7 +2383,6 @@ class ScvmmProvisionProvider extends AbstractProvisionProvider implements Worklo
             computeServer.status = 'provisioned'
             computeServer.statusMessage = resizeError
             computeServer = saveAndGet(computeServer)
-            rtn.success = false
             rtn.setError("${e}")
         }
         return rtn
